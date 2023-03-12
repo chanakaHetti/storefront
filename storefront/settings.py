656091f8c88a1a7830ec5bcd72eb97f7e3ff97fb
Chanaka Hettiarachchi <chanakahettiarachchi@Chanakas-MacBook-Air.local>
2023-03-12 17:07:31 +0000

    Connected with mysql

--- storefront/settings.py
+++ storefront/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'USER': 'root',
+        'PASSWORD': '123456789',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
