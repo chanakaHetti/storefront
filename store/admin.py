672dabc1c27c7fabef22cdce567f80a067eb203b
Chanaka Hettiarachchi <chanakahettiarachchi@Chanakas-MacBook-Air.local>
2023-03-16 23:54:43 +0000

    Admin module Children inline

--- store/admin.py
+++ store/admin.py
@@ -29,6 +29,7 @@ class ProductAdmin(admin.ModelAdmin):
     list_editable = ['unit_price']
     list_filter = ['collection', 'last_update', InventoryFilter]
     list_per_page = 10
+    search_fields = ['title']
 
     @admin.display(ordering='inventory')
     def inventory_status(self, product):
@@ -74,6 +75,15 @@ class CollectionAdmin(admin.ModelAdmin):
             product_count=Count('product')
         )
 
+class OrderItemInlie(admin.TabularInline):
+    autocomplete_fields = ['product']
+    min_num = 1
+    max_num = 10
+    model = models.OrderItem
+    extra = 0
+
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['customer']
+    inlines = [OrderItemInlie]
     list_display = ['id', 'placed_at', 'customer']
