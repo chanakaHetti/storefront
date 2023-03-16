0ed30a6a270020ec16f531b3449b2836e6d0ddd5
Chanaka Hettiarachchi <chanakahettiarachchi@Chanakas-MacBook-Air.local>
2023-03-16 22:57:36 +0000

    Admin module filter

--- store/admin.py
+++ store/admin.py
@@ -4,10 +4,25 @@ from django.utils.html import format_html, urlencode
 from django.urls import reverse
 from . import models
 
+class InventoryFilter(admin.SimpleListFilter):
+    title = 'inventory' # title of the filter
+    parameter_name = 'inventory' # quey string parameter on the url
+
+    def lookups(self, request, model_admin): # What items should appear filter list
+        return [
+            ('<10', 'Low')
+        ]
+    
+    def queryset(self, request, queryset): # This is the filtering logic
+        if self.value() == '<10':
+            return queryset.filter(inventory__lt=10)
+        
+
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'unit_price', 'inventory_status']
     list_editable = ['unit_price']
+    list_filter = ['collection', 'last_update', InventoryFilter]
     list_per_page = 10
 
     @admin.display(ordering='inventory')
@@ -21,6 +36,7 @@ class CustomerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'membership']
     list_editable = ['membership']
     list_per_page = 10
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
 
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
